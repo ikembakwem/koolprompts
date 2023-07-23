d3941d5d2dbd61151bf97baabfc85286fcd56a0a
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-23 23:20:02 +0100

    Added hero title and sub-title

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and share
+        <br className="max-md:hidden" />{" "}
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+
+      <p className="desc text-center">
+        Koolprompt is an open source modern tool for discoovering and creating
+        awesome prompts
+      </p>
+
+      {/* TODO - Feed */}
+    </section>
+  );
 };
 export default Home;
